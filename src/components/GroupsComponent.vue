73ae61851f0280c8836fed71a4734d3afe27c39f
Fatyusha <124563888+Fatyusha@users.noreply.github.com>
2024-09-17 13:52:06 +0300

    Group

---
+++ src/components/GroupsComponent.vue
@@ -0,0 +1,66 @@
+<script>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';
+import Row from 'primevue/row';
+export default {
+  components: {
+    DataTable,
+    Column,
+    ColumnGroup,
+    Row,
+  },
+  props: {
+    n_group: Number,
+    players: [],
+  },
+  data() {
+    return{
+      list: {},
+    }
+  },
+  methods: {
+    parse() {
+      let dictionary = {};
+      let liters = ['A', "B", "C", "D", "E", "F", "G", "H"];
+      for(let i = 0; i < this.n_group; i++){
+        dictionary[liters[i]] = []
+        this.players.forEach(function(player) {
+          if (player.group_name === liters[i]){
+            dictionary[liters[i]].push(player);
+          }
+        });
+
+      }
+      return dictionary;
+    }
+  },
+  beforeMount() {
+    this.list = this.parse()
+  }
+}
+</script>
+
+<template>
+  <div class="flex flex-wrap" style="display: flex; flex-wrap: wrap; max-width: 100%; background-color: white; border: 2px solid yellow">
+    <DataTable v-for="i in list" :value="i" class="bg-white"
+               showGridlines sortField="quantity" :sortOrder="-1" style="border: 2px solid black;  border-radius: 10px; padding: 3%; margin: 0; width: 50%">
+      <template #header>
+        <div class="flex justify-between">
+          Группа {{i[0].group_name}}
+        </div>
+      </template>
+      <Column header="№">
+        <template #body="slotProps">
+          {{ slotProps.index + 1 }}
+        </template>
+      </Column>
+      <Column field="name" header="Участник"></Column>
+      <Column field="score" header="Набранные очки"></Column>
+    </DataTable>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
