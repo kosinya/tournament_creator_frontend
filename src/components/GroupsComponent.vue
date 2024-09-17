8b3a563c13ac5eb1fe40362918d017f684f25f51
Kougami <ko.sinya@yandex.ru>
2024-09-17 14:21:13 +0300

    commit 13

--- src/components/GroupsComponent.vue
+++ src/components/GroupsComponent.vue
@@ -42,9 +42,8 @@ export default {
 </script>
 
 <template>
-  <div class="flex flex-wrap" style="display: flex; flex-wrap: wrap; max-width: 100%; background-color: white; border: 2px solid yellow">
-    <DataTable v-for="i in list" :value="i" class="bg-white"
-               showGridlines sortField="quantity" :sortOrder="-1" style="border: 2px solid black;  border-radius: 10px; padding: 3%; margin: 0; width: 50%">
+  <div class="flex justify-content-between flex-wrap border-transparent bg-gray-100" style="display: flex; flex-wrap: wrap;">
+    <DataTable v-for="i in list" :value="i" showGridlines sortField="quantity" :sortOrder="-1" style="border: 2px solid transparent;  border-radius: 10px; padding: 3%; margin: 0; width: 50%">
       <template #header>
         <div class="flex justify-between">
           Группа {{i[0].group_name}}
@@ -56,7 +55,7 @@ export default {
         </template>
       </Column>
       <Column field="name" header="Участник"></Column>
-      <Column field="score" header="Набранные очки"></Column>
+      <Column field="quantity" header="Набранные очки"></Column>
     </DataTable>
   </div>
 </template>
--- src/components/LeagueInfo.vue
+++ src/components/LeagueInfo.vue
@@ -1,9 +1,100 @@
 <script>
+import GroupsComponent from "./GroupsComponent.vue";
+import ScrollPanel from 'primevue/scrollpanel';
 export default {
   data() {
     return {
-
+      players: [
+        {
+          code:'dffbf',
+          name:'dfbdb',
+          category:'Fitness',
+          quantity: 7,
+          group_name: 'B',
+        },
+        {
+          code:'dffbf',
+          name:'better',
+          category:'Fitness',
+          quantity: 10,
+          group_name: 'A',
+        },
+        {
+          code:'dffbf',
+          name:'cool',
+          category:'Fitness',
+          quantity: 8,
+          group_name: 'E',
+        },
+        {
+          code:'dffbf',
+          name:'hey',
+          category:'Fitness',
+          quantity: 4,
+          group_name: 'F',
+        },
+        {
+          code:'dffbf',
+          name:'hey',
+          category:'Fitness',
+          quantity: 4,
+          group_name: 'G',
+        },
+        {
+          code:'dffbf',
+          name:'hey',
+          category:'Fitness',
+          quantity: 4,
+          group_name: 'C',
+        },
+        {
+          code:'dffbf',
+          name:'dfbdb',
+          category:'Fitness',
+          quantity: 7,
+          group_name: 'D',
+        },
+        {
+          code:'dffbf',
+          name:'dfbdb',
+          category:'Fitness',
+          quantity: 7,
+          group_name: 'D',
+        },
+        {
+          code:'dffbf',
+          name:'dfbdb',
+          category:'Fitness',
+          quantity: 7,
+          group_name: 'D',
+        },
+        {
+          code:'dffbf',
+          name:'dfbdb',
+          category:'Fitness',
+          quantity: 7,
+          group_name: 'D',
+        },
+        {
+          code:'dffbf',
+          name:'dfbdb',
+          category:'Fitness',
+          quantity: 7,
+          group_name: 'H',
+        },
+        {
+          code:'dffbf',
+          name:'dfbdb',
+          category:'Fitness',
+          quantity: 7,
+          group_name: 'B',
+        },
+      ],
     }
+  },
+  components: {
+    GroupsComponent,
+    ScrollPanel
   },
   computed: {
     currentLeague() {
@@ -17,6 +108,9 @@ export default {
   <div class="flex justify-content-start align-items-start h-full w-full pb-3">
     <div class="flex flex-column mx-3 p-3 bg-gray-100 border-gray-100 border-round-xl h-full w-full">
       <label v-if="currentLeague != null" class="text-xl font-semibold mb-2">{{currentLeague.name}}</label>
+      <ScrollPanel class="max-h-full p-3">
+        <GroupsComponent v-bind:players="this.players" v-bind:n_group=8 />
+      </ScrollPanel>
     </div>
   </div>
 </template>
